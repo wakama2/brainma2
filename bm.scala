582eb668538c92965ecee216d92958b554147967
wakamatsu <yuuki.wakamatsu@gmail.com>
2012-06-23 02:11:05 +0900

    modify eval

--- bm.scala
+++ bm.scala
@@ -5,14 +5,19 @@ object BrainMa2 {
 
 	case class Op {
 		def merge(op: Op): Option[Op] = None
+		def eval(sp: Int, stack: Array[Int]): Int = sp
 	}
 	case class Nop extends Op {
 		override def merge(op: Op): Option[Op] = Some(op)
+		override def eval(sp: Int, stack: Array[Int]): Int = sp
 	}
 	case class AddOp(n: Int) extends Op {
 		override def merge(op: Op): Option[Op] = op match {
 			case AddOp(m) => Some(AddOp(n + m))
 			case _ => None
+		}
+		override def eval(sp: Int, stack: Array[Int]): Int = {
+			stack(sp) += n; sp
 		}
 	}
 	case class ShiftOp(n: Int) extends Op {
@@ -20,30 +25,43 @@ object BrainMa2 {
 			case ShiftOp(m) => Some(ShiftOp(n + m))
 			case _ => None
 		}
+		override def eval(sp: Int, stack: Array[Int]): Int = sp + n
 	}
-	case class InputOp extends Op
-	case class OutputOp extends Op
-	case class BlockOp(code: List[Op]) extends Op
-
-	def addop(b: ListBuffer[Op], op: Op) =
-		if(b.size > 0) b(b.size-1).merge(op) match {
-			case Some(o) => b(b.size-1) = o
-			case None => b += op
-		} else {
-			b += op
+	case class InputOp extends Op {
+		override def eval(sp: Int, stack: Array[Int]): Int = sp/*TODO*/
+	}
+	case class OutputOp extends Op {
+		override def eval(sp: Int, stack: Array[Int]): Int = {
+			print(stack(sp).toChar); sp
 		}
+	}
+	case class BlockOp(code: List[Op]) extends Op {
+		override def eval(sp0: Int, stack: Array[Int]): Int = {
+			var sp = sp0
+			while(stack(sp) != 0) code.foreach { c => sp = c.eval(sp, stack) }
+			sp
+		}
+	}
 
 	def compile(src: Iterator[Char]): List[Op] = {
 		val b = new ListBuffer[Op]
 		var e = true
+		def addop(op: Op) =
+			if(b.size > 0) b(b.size-1).merge(op) match {
+				case Some(o) => b(b.size-1) = o
+				case None => b += op
+			} else {
+				b += op
+			}
+
 		while(e && src.hasNext) src.next match {
-			case '+' => addop(b, AddOp(1))
-			case '-' => addop(b, AddOp(-1))
-			case '<' => addop(b, ShiftOp(-1))
-			case '>' => addop(b, ShiftOp(1))
-			case '.' => addop(b, OutputOp())
-			case ',' => addop(b, InputOp())
-			case '[' => addop(b, BlockOp(compile(src)))
+			case '+' => addop(AddOp(1))
+			case '-' => addop(AddOp(-1))
+			case '<' => addop(ShiftOp(-1))
+			case '>' => addop(ShiftOp(1))
+			case '.' => addop(OutputOp())
+			case ',' => addop(InputOp())
+			case '[' => addop(BlockOp(compile(src)))
 			case ']' => e = false; 
 			case _   => 
 		}
@@ -59,48 +77,29 @@ object BrainMa2 {
 		}
 	}
 
-	def run(code: List[Op]) {
-		val stack = new Array[Int](1024)
-		var sp = 0
-		def runBlock(code: List[Op]) {
-			code.foreach {
-				case Nop()      =>
-				case AddOp(n)   => stack(sp) += n
-				case ShiftOp(n) => sp += n
-				case InputOp()  => /* TODO */
-				case OutputOp() => print(stack(sp).toChar)
-				case BlockOp(c) => while(stack(sp) != 0) runBlock(c)
-			}
-		}
-		runBlock(code)
-	}
+	var showCode = false
 
-	class Context {
-		var showCode = false
-	}
-
-	def eval(src: Iterator[Char], ctx: Option[Context] = None) {
+	def eval(src: Iterator[Char]) {
 		var code = compile(src)
-		ctx.foreach { c =>
-			if(c.showCode) {
-				println("*--------------------*")
-				dump(code)
-				println("*--------------------*")
-			}
+		if(showCode) {
+			println("*--------------------*")
+			dump(code)
+			println("*--------------------*")
 		}
-		run(code)
+		var sp = 0
+		val stack = new Array[Int](1024)
+		code foreach { c => sp = c.eval(sp, stack) }
 	}
 
 	def main(args: Array[String]) {
 		var fileName: Option[String] = None
-		val ctx = new Context
 		args.foreach {
 			case "-v" => println("brainma2 version 2.0"); return
-			case "-i" => ctx.showCode = true
+			case "-i" => showCode = true
 			case s    => fileName = Some(s)
 		}
 		fileName match {
-			case Some(f) => eval(Source.fromFile(f), Some(ctx))
+			case Some(f) => eval(Source.fromFile(f))
 			case None    => println("no input file")
 		}
 	} 
